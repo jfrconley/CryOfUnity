5819f7f0b47003b4348d947da1a84eebf844ca18
Coal <cole.wilfert@gmail.com>
2018-06-22 16:55:14 -0500

    Player Movement/Physics
    Player collisions with a new crate object, 3D perspective, 2D colliders, some materials

---
+++ Assets/Scripts/PlayerInputControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerInputControl : MonoBehaviour
+{
+	//Component References
+	private new Rigidbody2D rigidbody;
+	
+	//Serialized
+	[SerializeField] private float moveSpeed = 1;
+	
+	private void Awake () {
+		rigidbody = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	private void Update ()
+	{
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+		rigidbody.velocity = input * moveSpeed * 100 * Time.deltaTime;
+	}
+}
