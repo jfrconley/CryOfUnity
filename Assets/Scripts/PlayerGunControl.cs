e36cb8ff64747488d7e85aedecf6f293d0b49683
john.conley <john.conley@modopayments.com>
2018-06-24 15:50:11 -0500

    Basic bullet syncing and gun registry

--- Assets/Scripts/PlayerGunControl.cs
+++ Assets/Scripts/PlayerGunControl.cs
@@ -1,20 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using Random = UnityEngine.Random;
 
-public class PlayerGunControl : MonoBehaviour {
-
-    GunSettings gun;
+[RequireComponent(typeof(NetworkIdentity))]
+[RequireComponent(typeof(PlayerNetworkManager))]
+public class PlayerGunControl : NetworkBehaviour
+{
+    [SyncVar (hook = "UpdateGun")] private GunLocker.GunState currentGun;
+    GunSettings currentGunSetting;
+    private PlayerNetworkManager _networkManager;
     [SerializeField] Transform bulletSpawn;
     private float cooldown = 0.4f;
+    private bool _isInit;
 
-    private void Start()
+    private void Awake()
     {
-        gun = GunLocker.singleton.GetGun("Pistol");
+        _networkManager = gameObject.GetComponent<PlayerNetworkManager>();
+    }
+
+    private void Init()
+    {
+        if (isLocalPlayer && _networkManager.PlayerId != "" && !_isInit)
+        {
+            Debug.Log("Requesting Starting gun Pistol");
+            _isInit = true;
+            CmdRequestStartingGun("Pistol");
+        }
+    }
+
+    [Command]
+    private void CmdRequestStartingGun(string type)
+    {
+        string id = GunLocker.singleton.ManufactureGun(type);
+        GunLocker.singleton.ChangeOwnership(id, _networkManager.PlayerId);
+        currentGun = GunLocker.singleton.RetrieveGun(id);
+    }
+
+    private void UpdateGun(GunLocker.GunState gun)
+    {
+        Debug.Log($"Updating gun to {gun.Id} for player {_networkManager.PlayerId}");
+        currentGun = gun;
+        currentGunSetting = GunLocker.singleton.GetGunInfo(gun.Type);
     }
 
     private void Update()
     {
+        Init();
         cooldown -= Time.deltaTime;
     }
 
@@ -27,32 +61,60 @@ public class PlayerGunControl : MonoBehaviour {
 
     public void TriggerHeld()
     {
-        if (gun.fireMode == GunSettings.GunFireMode.Semi)
+        if (currentGunSetting.fireMode == GunSettings.GunFireMode.Semi)
             return;
         if (cooldown > 0)
             return;
         Shoot();
     }
 
+    [Command]
+    public void CmdRequestFire(Vector3 position, Quaternion rotation, Quaternion offset)
+    {
+        RpcDoFire(position, rotation, offset);
+    }
+
+    [ClientRpc]
+    public void RpcDoFire(Vector3 position, Quaternion rotation, Quaternion offset)
+    {
+        if (!isLocalPlayer)
+        {
+            DoFire(position, rotation, offset);
+        }
+    }
+
+    public void DoFire(Vector3 position, Quaternion rotation, Quaternion offset)
+    {
+        GameObject flash = Instantiate(currentGunSetting.muzzleFlashPrefab, bulletSpawn.position, bulletSpawn.rotation, bulletSpawn);
+        Destroy(flash, currentGunSetting.muzzleTimer);
+
+        GameObject go = Instantiate(currentGunSetting.bulletPrefab, position, rotation * offset);
+    }
+    
     void Shoot()
     {
-        cooldown = gun.fireRate;
-        //ammo
+        if (currentGun.Type != "" && isLocalPlayer)
+        {
+            cooldown = currentGunSetting.fireRate;
+            //ammo
 
-        //MuzzleFlash
-        GameObject flash = Instantiate(gun.muzzleFlashPrefab, bulletSpawn.position, bulletSpawn.rotation, bulletSpawn);
-        Destroy(flash, gun.muzzleTimer);
+            //MuzzleFlash
+//            GameObject flash = Instantiate(currentGunSetting.muzzleFlashPrefab, bulletSpawn.position,
+//                bulletSpawn.rotation, bulletSpawn);
+//            Destroy(flash, currentGunSetting.muzzleTimer);
 
-        Quaternion rotOffset = Quaternion.AngleAxis(Random.Range(-gun.bulletSpread / 2, gun.bulletSpread / 2), Vector3.up);//adjust upward rotation Quaternion.AngleAxis(Random.Range(-gun.bulletSpread / 2, gun.bulletSpread / 2), Vector3.right)
-        GameObject go = Instantiate(gun.bulletPrefab, bulletSpawn.position, bulletSpawn.rotation * rotOffset);
+            Quaternion rotOffset =
+                Quaternion.AngleAxis(
+                    Random.Range(-currentGunSetting.bulletSpread / 2, currentGunSetting.bulletSpread / 2),
+                    Vector3.up); //adjust upward rotation Quaternion.AngleAxis(Random.Range(-gun.bulletSpread / 2, gun.bulletSpread / 2), Vector3.right)
+//            GameObject go = Instantiate(currentGunSetting.bulletPrefab, bulletSpawn.position,
+//                bulletSpawn.rotation * rotOffset);
+            
+            CmdRequestFire(bulletSpawn.position, bulletSpawn.rotation, rotOffset);
+            DoFire(bulletSpawn.position, bulletSpawn.rotation, rotOffset);
+        }
     
     //gun.shootSound;
     //gun.emptySound;
-}
-
-    public class GunVariables
-    {
-        public string name;
-        public int ammo = 0;
     }
 }
