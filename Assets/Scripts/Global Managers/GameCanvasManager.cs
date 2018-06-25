be7efbc60893ec38f913cf888d79691351067e5c
Coal <cole.wilfert@gmail.com>
2018-06-25 14:40:32 -0500

    Health Bar
    See how much you hurt. FeelsBadMan

---
+++ Assets/Scripts/Global Managers/GameCanvasManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameCanvasManager : MonoBehaviour {
+    
+    public static GameCanvasManager singleton;
+
+    [SerializeField] Slider healthSlider;
+    GameObject fillObject;
+
+	void Awake ()
+    {
+		if (singleton == null)
+        {
+            singleton = this;
+            fillObject = healthSlider.fillRect.gameObject;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+	
+    public void SetHealthMaximum(int max)
+    {
+        healthSlider.maxValue = max;
+        healthSlider.value = max;
+    }
+
+	public void SetHealth(int value)
+    {
+        healthSlider.value = value;
+        if (value == 0)
+            fillObject.SetActive(false);
+        else if (!fillObject.activeSelf)
+        {
+            fillObject.SetActive(true);
+        }
+    }
+}
